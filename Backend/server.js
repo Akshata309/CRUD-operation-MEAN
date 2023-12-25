16df527e8edd6dbaa58b0a22d3bdc1c19a6fd639
Akshata309 <akshatakhadake309@gmail.com>
2023-12-25 12:36:20 +0530

    commit

--- Backend/server.js
+++ Backend/server.js
@@ -1,23 +1,29 @@
-const express = require("express");
-const server = express();
-const mongoose = require("mongoose");
+var express = require('express');
+var server = express();
+var routes = require('./routes/routes');
+var mongoose = require('mongoose');
+const cors = require('cors');
 
-mongoose.connect("mongodb://localhost:27017/StudentRegistryDB", {
-    useNewUrlParser: true,    
+mongoose
+  .connect("mongodb://localhost:27017/StudentRegistryDB", {
+    useNewUrlParser: true,
     useUnifiedTopology: true,
-})
+  })
   .then(() => {
-    console.log("DB connected!");
+    console.log("DB Connectedddd!!!!!!!!!!!");
+
+    server.use(cors());
+    server.use(express.json());
+    server.use(routes);
+
+    server.listen(8000, function check(error) {
+      if (error) {
+        console.log("errorr");
+      } else {
+        console.log("startedddddd");
+      }
+    });
   })
   .catch((error) => {
     console.error("DB connection error:", error);
   });
-
-server.use(express.json());
-server.listen(8000, function check(error) {
-  if (error) {
-    console.log("error");
-  } else {
-    console.log("startedddd");
-  }
-});
